3a5a43313c1ebc99f792c77b45e0b6570be07944
gerrygeko <lamorte.gerardo@gmail.com>
2019-05-07 19:31:52 +0200

    Option to select other results with keyboard input

--- handlers/namedsearchhandler.py
+++ handlers/namedsearchhandler.py
@@ -1,18 +1,79 @@
-from telegram import InputMediaPhoto
+from telegram import InputMediaPhoto, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import ConversationHandler, MessageHandler, Filters, CommandHandler
 
 import logger
 from scryfallapi import api
 
-START, CHOOSING, TYPING_CHOICE = range(3)
+START, CHOOSING, PICKING,  = range(3)
 log = logger.get_logger()
-fallbacks=[]
+fallback = []
 
 
 def search(bot, update):
-    bot.send_message(chat_id=update.message.chat_id,
-                     text="Type the name of the card you want to search. It doesn't need to be the exact name")
+    send_text_message(bot, update, "Type the name of the card you want to search. It doesn't need to be the exact name")
     return CHOOSING
+
+
+def get_named_cards(bot, update):
+    card_name = update.message.text
+    log.info('User is searching for the card %s', card_name)
+    most_expansive_card_list, double_faced = api.get_most_expansive_card(card_name)
+    card_list_by_name = api.get_list_card_by_name(card_name)
+    if len(most_expansive_card_list) == 0:
+        bot.send_message(chat_id=update.message.chat_id, text="No cards found with the text that you typed, type a new "
+                                                              "name")
+        return CHOOSING
+    else:
+        if not double_faced:
+            send_picture(bot, update, most_expansive_card_list[0])
+        else:
+            media = create_media_group(most_expansive_card_list)
+            bot.send_media_group(chat_id=update.message.chat_id, media=media)
+        send_message_with_card_cost(bot, update, most_expansive_card_list[0])
+        if len(card_list_by_name) > 0:
+            send_message_with_keyboard(bot, update, create_keyboard_from_card_list(card_list_by_name),
+                                       "Other cards that also contains the text you provided: ", one_time_use=True)
+            return PICKING
+        return START
+
+
+def send_message_with_keyboard(bot, update, keyboard, message, one_time_use):
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_use=one_time_use)
+    bot.send_message(chat_id=update.message.chat_id, text=message, reply_markup=reply_markup)
+
+
+def send_message_with_card_cost(bot, update, card, remove_keyboard=False):
+    print(card)
+    reply_markup = ReplyKeyboardRemove(remove_keyboard=False)
+    if 'eur' in card.keys():
+        cost_message = "The cost of this card is: " + card['eur'] + " euro."
+    else:
+        cost_message = "This card has no cost"
+    if remove_keyboard:
+        reply_markup.remove_keyboard = True
+    bot.send_message(chat_id=update.message.chat_id, text=cost_message, reply_markup=reply_markup)
+
+
+def send_text_message(bot, update, message):
+    bot.send_message(chat_id=update.message.chat_id, text=message)
+
+
+def send_picture(bot, update, card):
+    bot.send_photo(chat_id=update.message.chat_id, photo=card['image_uris']['normal'])
+
+
+def get_specific_card(bot, update):
+    card_name = update.message.text
+    log.info('User is searching for the specific card %s', card_name)
+    card = api.get_specific_card(card_name)
+    send_picture(bot, update, card)
+    send_message_with_card_cost(bot, update, card, True)
+    return START
+
+
+def search_interrupted(bot, update):
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="Search interrupted.")
 
 
 # Method to create a media group to display more images if the card is composed by more
@@ -25,34 +86,25 @@ def create_media_group(card_list):
     return media
 
 
-def get_named_cards(bot, update):
-    card_name = update.message.text
-    log.info('User is searching for the card %s', card_name)
-    card_list, double_faced = api.get_most_expansive_card(card_name)
-    if len(card_list) == 0:
-        bot.send_message(chat_id=update.message.chat_id, text="No cards found with the text that you typed, type a new "
-                                                              "name")
-        return CHOOSING
-    else:
-        if not double_faced:
-            card_image = card_list[0]['image_uris']['normal']
-            bot.send_photo(chat_id=update.message.chat_id, photo=card_image)
+def create_keyboard_from_card_list(card_list):
+    keyboard = []
+    row = None
+    first_element = True
+    for i in range(0, len(card_list)):
+        if first_element:
+            row = [KeyboardButton(text=card_list[i]['name'])]
+            first_element = False
         else:
-            media = create_media_group(card_list)
-            bot.send_media_group(chat_id=update.message.chat_id, media=media)
-        cost_message = 'The cost of this card is: ' + card_list[0]['eur'] + ' euro'
-        bot.send_message(chat_id=update.message.chat_id, text=cost_message)
-        return START
-
-
-def search_interrupted(bot, update):
-    bot.send_message(chat_id=update.message.chat_id,
-                     text="Search interrupted.")
+            row.append(KeyboardButton(text=card_list[i]['name']))
+            keyboard.append(row)
+            first_element = True
+    return keyboard
 
 
 states = {
     START: [CommandHandler('search', search)],
     CHOOSING: [MessageHandler(Filters.text, get_named_cards)],
+    PICKING: [MessageHandler(Filters.text, get_specific_card)],
 }
 
 
@@ -68,7 +120,7 @@ class NamedSearchHandler(ConversationHandler):
                  conversation_timeout=None):
         self.states = states
         self.entry_points = [CommandHandler('search', search)]
-        self.fallbacks = fallbacks
+        self.fallbacks = fallback
         self.allow_reentry = allow_reentry
         self.run_async_timeout = run_async_timeout
         self.timed_out_behavior = timed_out_behavior
--- scryfallapi/api.py
+++ scryfallapi/api.py
@@ -18,35 +18,27 @@ def get_random_card():
     return json_card
 
 
-def get_named_card(name):
+def get_list_card_by_name(name):
     response = requests.get(SCRYFALL_URL + NAMED_ENDPOINT + name)
     json_data = json.loads(response.text)
     json_cards = []
     if json_data['object'] != 'error':
-        json_cards_to_order = json_data['data']
-        # Sorting the list of cards (still doesn't work)
-        json_cards = sorted(json_cards_to_order, key=lambda k: ("eur" not in k, k.get("eur", None)), reverse=True)
-        cards_discarded = []
-        cards_not_discarded = []
-        print('------------List before------------')
-        for card_dict in json_cards:
-            print(card_dict['name'])
-            if 'eur' in card_dict.keys():
-                print(card_dict['eur'])
-        for card_dict in json_cards:
-            if 'eur' not in card_dict.keys():
-                print('Discarding this card')
-                print(card_dict['name'])
-                cards_discarded.append(card_dict)
-            else:
-                cards_not_discarded.append(card_dict)
-                print('Not discarding this')
-                print(card_dict['name'])
-                print(card_dict['eur'])
-        json_cards = cards_not_discarded + cards_discarded
-        print('------------List ordered------------')
-        for card_dict in json_cards:
-            print(card_dict['name'])
+        for card in json_data['data']:
+            json_cards.append(card)
+    else:
+        log.error('No cards were found')
+        return []
+    return json_cards
+
+
+def get_specific_card(name):
+    response = requests.get(SCRYFALL_URL + NAMED_ENDPOINT + name)
+    json_data = json.loads(response.text)
+    if json_data['object'] != 'error':
+        return json_data['data'][0]
+    else:
+        log.error('No cards were found')
+        return []
     return json_cards
 
 
@@ -76,11 +68,10 @@ def get_most_expansive_card(name):
     else:
         log.error('No cards were found')
         return [], False
-    if 'card_faces' in json_cards[0].keys():
+    if 'card_faces' in json_cards[0].keys() and 'image_uris' not in json_cards[0].keys():
         log.info('Found double-faced card')
-        new_json_cards = []
-        new_json_cards.append(json_cards[0]['card_faces'][0])
-        new_json_cards.append(json_cards[0]['card_faces'][1])
-        new_json_cards[0]['eur'], new_json_cards[1]['eur'] = json_cards[0]['eur']
+        new_json_cards = [json_cards[0]['card_faces'][0], json_cards[0]['card_faces'][1]]
+        new_json_cards[0]['eur'] = json_cards[0]['eur']
         return new_json_cards, True
     return json_cards, False
+
